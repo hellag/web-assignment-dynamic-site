009069b74272977400a780efd842f379363287c3
Elena Lagiou <elena@cliomuseapp.com>
2022-12-19 11:09:15 +0200

    add some table

--- js/main.js
+++ js/main.js
@@ -7,25 +7,42 @@ function getResults() {
 
 function displayHeader(data) {
     $('#header-title').empty();
-    $('#header-title').append("<span>"+data+"</span><br/>")
+    $('#header-title').append("<span>" + data + "</span><br/>")
 }
 
 function displayResults(data) {
     $('#results').empty();
+    const element = document.createElement("tr");
+    const element2 = document.createElement("tr");
+    element.setAttribute("id", "sub-result");
+    element2.setAttribute("id", "header-sub-result");
+    $('#results').append(element2);
+    $('#results').append(element);
     $.each(data, function (key, value) {
         console.log(value.id);
-        $('#results')
+        $('#header-sub-result')
+            .append($("<th></th>")
+                .attr("value", key)
+                .text(value.winnerParticipant + "/" + value.loosingParticipant));
+    });
+
+    $.each(data, function (key, value) {
+        console.log(value.id);
+        $('#sub-result')
             .append($("<td></td>")
                 .attr("value", key)
-                .text(value.winnerScore+" - "+value.losingScore));
+                .text(value.winnerScore + " - " + value.losingScore));
     });
 
 }
 
 function getGrading() {
     displayHeader("Βαθμολογία");
+    $('#results').empty();
 
-
+}
+function displayGrading() {
+    $('#results').empty();
 
 }
 
@@ -33,9 +50,21 @@ function calculateResults() {
 
 }
 
-
+function displayProgram(data) {
+    $('#results').empty();
+    $.each(data, function (key, value) {
+        console.log(value.id);
+        $('#results')
+            .append($("<span></span><br/>")
+                .attr("value", key)
+                .text(value.winnerParticipant + "/" + value.loosingParticipant + " - " + value.dateMatch));
+    });
+}
 
 function getProgram() {
     displayHeader("Πρόγραμμα");
+    fetch('./results.json')
+        .then((response) => response.json())
+        .then((json) => displayProgram(json.results))
 
 }
